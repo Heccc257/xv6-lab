bd878d031dcf623dd83bf6cad0a90782d7ae67e2
HCH <2762312941@qq.com>
2023-03-13 16:53:49 +0800

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+
+    int sleep_time = atoi(argv[1]);
+    sleep(sleep_time);
+    exit(0);
+}
